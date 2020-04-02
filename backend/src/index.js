52251b1c91a0577db248676ccc385fe3430db64a
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-04-02 14:14:52 -0300

    Adding validation

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,26 +1,14 @@
 // importar o modulo express para a variavel express
 const express = require('express');
 const cors = require('cors');
+const { errors } =  require('celebrate');
 const routes = require('./routes');
 
-// variavel que vai armacenar a aplicação
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-// criar rota raiz
-// app.get('/',(request, response) => {
-//     return response.send('Hello world!');
-// });
-
-// criar rota raiz com json
-// app.get('/', (request, response) => {
-//     return response.json({
-//         evento: 'Semama OmniStack 11.0',
-//         aluno: 'Fernanda Rodriguez'
-//     });
-// });
-// localhost:3333
 app.listen(3333);
---
+++ notes/dia_05.md
@@ -0,0 +1,25 @@
+# Quinto dia
+
+* Adicionando validação
+	* `npm install celebrate`
+
+* Adicionando testes
+	* Por que fazer testes?
+	* Configurando Jest
+	* Tipos de testes
+	* Configurando banco de testes
+	* Instalando supertest
+	* Testando rota de autenticação
+
+* Deploy
+	* Alternativas
+	* Qual escolher?
+
+* Estudos daqui pra frente
+	* Padrões de código: ESLint, Prettier
+	* Autenticação JWT
+	* Styled Components 
+
+* Dicas para aproveitar melhor
+	* Github
+	* LinkedIn
